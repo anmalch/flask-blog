20e4577325b2736c3cfc8a10cbc599dc7c9cbfb5
Anna Malchikova <74615062+anmalch@users.noreply.github.com>
2024-09-11 12:14:33 +0200

    Update comments

--- malchikova_flask_blog/models.py
+++ malchikova_flask_blog/models.py
@@ -20,12 +20,12 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    #скрываем id от злоумышлиников
+    # Hiding the ID from attackers
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
-    #распаковываем id и сравниваем тот ли он
+    # Unpacking the ID and comparing if it's the correct one
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
